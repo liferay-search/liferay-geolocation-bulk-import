9fb014c1090a685e0244322ced8e66a12a505697
André de Oliveira <andre.oliveira@liferay.com>
2018-11-30 15:21:28 -0800

    Break away from master SDK

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -o errexit ; set -o nounset
 
-${LIFERAY_PORTAL_DIR}/gradlew deploy
+GRADLEW_DIR=../..
+
+${GRADLEW_DIR}/gradlew deploy
 
 cp build/libs/*.jar ${LIFERAY_BUNDLES_PORTAL_DIR}/deploy/
